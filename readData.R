8ceaab680f7975c2edfe0aee46305c5bdc096daa
TugbaSari <betitub@hotmail.com>
2017-05-13 16:43:47 +0200

    Create a generic read function for file household power consumption
    - It creates a function called `readHouseholPowerConsumptionData`
    - It allows to choose the columns as optional parameter

---
+++ readData.R
@@ -0,0 +1,16 @@
+
+## Use sqldf package to filter out unnecessary rows during read to improve read speed and memory usage.
+if("sqldf" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("sqldf")
+  library(sqldf)
+}
+
+readHouseholdPowerConsumptionData <- function(columns = "*") {
+  ## read power consumption data for 2 days
+  read.csv.sql("household_power_consumption.txt",
+                  sql = paste("select ", columns, " from file where Date in ('1/2/2007', '2/2/2007')"),
+                  header = TRUE, 
+                  sep = ";"
+              )
+}
+
